16c18055ce555ee4ca4ef30c265e9609885f8269
ShubhamMewara <shubhammewara12@gmail.com>
2024-08-12 19:49:45 +0530

    added gpt-4o-mini

--- app/api/chat/azure/route.ts
+++ app/api/chat/azure/route.ts
@@ -29,6 +29,9 @@ export async function POST(request: Request) {
       case "gpt-4-vision-preview":
         DEPLOYMENT_ID = profile.azure_openai_45_vision_id || ""
         break
+      case "gpt-4o-mini":
+        DEPLOYMENT_ID = profile.azure_openai_35_turbo_id || ""
+        break
       default:
         return new Response(JSON.stringify({ message: "Model not found" }), {
           status: 400
