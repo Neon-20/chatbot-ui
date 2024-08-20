2dc07c225affb02d289d9bc7db014b138c753cef
ShubhamMewara <shubhammewara12@gmail.com>
2024-08-20 11:41:15 +0530

    fixed ChatSettings

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -79,4 +79,10 @@ const GPT3_5Turbo: LLM = {
   }
 }
 
-export const OPENAI_LLM_LIST: LLM[] = [GPT4oMini]
+export const OPENAI_LLM_LIST: LLM[] = [
+  GPT4oMini,
+  GPT4o,
+  GPT4Vision,
+  GPT4,
+  GPT3_5Turbo
+]
