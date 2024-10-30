5ae5ea57c0f71fb2442e50fd4ef73822847f4803
Neon-20 <rajveer23pr@gmail.com>
2024-10-30 18:22:16 +0530

    removed downloadable file icon

--- components/utility/profile-settings.tsx
+++ components/utility/profile-settings.tsx
@@ -733,7 +733,7 @@ export const ProfileSettings: FC<ProfileSettingsProps> = ({}) => {
           <div className="flex items-center space-x-1">
             <ThemeSwitcher />
 
-            <WithTooltip
+            {/* <WithTooltip
               display={
                 <div>
                   Download Chatbot UI 1.0 data as JSON. Import coming soon!
@@ -746,7 +746,7 @@ export const ProfileSettings: FC<ProfileSettingsProps> = ({}) => {
                   onClick={exportLocalStorageAsJSON}
                 />
               }
-            />
+            /> */}
           </div>
 
           <div className="ml-auto space-x-2">
